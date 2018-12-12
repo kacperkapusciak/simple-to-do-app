d55517d291444df127a4c3861384423358d20179
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-12 21:37:39 +0100

    Hardcoded basic todos

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: "Do laundry"},
+      {id: 2, content: "Clean up my flat"},
+      {id: 3, content: "Learn maths for exam"}
+    ]
+  }
   render() {
     return (
       <div>
-        
+        <h1>Simple To-do app</h1>
       </div>
     );
   }
