6c0b27b2369b74d8cb09a6251df08f6cb7355b73
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-14 00:01:39 +0100

    Added Center and H1 component

---
+++ src/styles/Center.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Center = styled.div`
+  margin: 0 auto;
+  width: 600px;
+`;
+
+export default Center;
---
+++ src/styles/H1.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  display: flex;
+  justify-content: center;
+`;
+
+export default H1;
