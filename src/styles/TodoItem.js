2d236be388362db4f787f7760a19efc831469445
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-12-14 00:26:24 +0100

    Added styling to items from list

---
+++ src/styles/TodoItem.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const TodoItem = styled.span`
+  display: flex;
+  justify-content: center;
+  margin: 20px;
+  padding: 10px;
+  color: #181818;
+  border: 1px solid #ff9900;
+  border-radius: 5px;
+  background-color: #ffad33;
+`;
+
+export default TodoItem;
