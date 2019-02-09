74021f19bcbf57a87c60e7a5d1f6bf0aac41f8b8
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2019-02-09 17:36:34 +0100

    Testing (#5)
    * Added enzyme and enzyme-adapter-react-16 packages
    
    * Added tests to TodoList component
    
    * Added tests to App component

---
+++ src/components/TodoList.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import TodoList from "./TodoList";
+import TodoItem from "../styles/TodoItem";
+import Success from "../styles/Success";
+
+configure({ adapter: new Adapter() });
+
+describe("<TodoList />", () => {
+  it("should diplay one <TodoItem> for every to-do", () => {
+    const todos = [{ id: 1, content: "Todo1" }, { id: 2, content: "Todo2" }];
+    const component = shallow(<TodoList todos={todos} />);
+    expect(component.find(TodoItem).length).toEqual(2);
+  });
+
+  it("should display <Success> message when there are 0 todos", () => {
+    const todos = [];
+    const component = shallow(<TodoList todos={todos} />);
+    expect(component.find(Success).length).toEqual(1);
+  });
+});
